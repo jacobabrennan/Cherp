2d82b86144620a37a404f24014602294b47c27b6
Jacob A. Brennan <jacobabrennan@gmail.com>
2017-01-05 18:34:23 -0500

    Mostly done. Added: * basic server * Cherp submission page * Cherp "thanks!" page * Saving of cherps to text file
    TO DO:
    * Securely serve saved cherps

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# chirp
-Go ahead, no one will know.
+# Cherp
+Cherp lets your friends tell you positive stuff anonymously. Yup, that's it.
+
+## Getting started
+```
+// Clone the repository
+git clone https://github.com/jacobabrennan/Cherp.git
+
+// Move into project directory, install project with npm
+cd Cherp
+npm install
+
+// Start the server
+node app.js
+```
+
+The Cherp server will open on port 8080 by default. You can change this by editing line 2 of app.js
+
+## TO DO
+Add https support so I can ask the user for a password, and retrieve submitted cherps.
---
+++ app.js
@@ -0,0 +1,77 @@
+// ==== CONFIGURATION ==========================================================
+var port = 8080;
+// ==== DON'T EDIT ANYTHING BELOW THIS LINE ====================================
+
+
+
+//==== CHIRP APP ===============================================================
+/*
+ *  This file provides configuration and implementation of a nodejs web server.
+ *  This is the "entry point" for this project.
+ *  To run the server, execute the following command:
+ *      node app.js
+ *
+ *  You can then access the web client at http://localhost:port
+ *      where 'port' is the number specified in the above configuration.
+ */
+//==============================================================================
+
+
+
+// ==== APP REQUIREMENTS + GLOBAL VALUES =======================================
+var fileSystem  = require("fs");
+var connect     = require("connect");
+var serveStatic = require('serve-static');
+var bodyParser  = require('body-parser');
+//var favicon     = require('serve-favicon');
+var cherpPath   = 'cherp';
+// =============================================================================
+
+
+
+// ==== CONFIGURE APP ROUTES ===================================================
+var app = connect();
+//app.use(favicon(__dirname + '/public/rsc/img/favicon.ico'));
+app.use('/', serveStatic(__dirname+'/public', {'index': ['index.html', 'index.htm']}));
+app.use('/cherp', bodyParser.urlencoded({ extended: false }))
+app.use('/cherp', function (request, response, next){
+    switch (request.method) {
+    case 'GET':
+        // TODO: Require Authentication
+        response.setHeader('Content-Type', 'application/json');
+        fileSystem.readFile(cherpPath, 'utf8', function (error, data){
+            // TODO: Handle read errors, perhaps with 500 level http code.
+            //response.end(data);
+            response.end();
+        });
+        break;
+    case 'POST':
+        var cherpText = request.body.derp;
+        app.saveCherp(cherpText);
+        response.writeHead(303, {Location: '/thanks.html?cherp='+encodeURIComponent(cherpText)});
+        response.end();
+        break;
+    default:
+        response.writeHead(405);
+        response.end('Error 405: Method not supported.');
+    }
+});
+// =============================================================================
+
+
+
+// =============================================================================
+app.saveCherp = function (cherpText, callback){
+    var escapedCherp = JSON.stringify(cherpText)+',';
+    fileSystem.appendFile(cherpPath, escapedCherp, 'utf8');
+}
+// =============================================================================
+
+
+
+// ==== RUN TESTS AND START SERVER =============================================
+    app.listen(port);
+    console.log('Cherp Server Started on port '+port+'.')
+// =============================================================================
+
+
